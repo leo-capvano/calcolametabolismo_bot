1b7a4b89ea869864e04c7a5ea5f6b7acdd0bb8cb
leo-capvano <60476114+leo-capvano@users.noreply.github.com>
2020-02-06 09:42:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pprint import pprint
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     
-    if content_type == "text" && msg["text"] == "/calcola":
+    if msg["text"] == "/calcola":
         bot.sendMessage(chat_id, "Benvenuto, qui puoi calcolare il tuo metabolismo basale!\n") 
 
 bot = telepot.Bot("1005610092:AAHU9mg1-_qb7VB0SJ8XPtUN4CUaIPDjLwM")
